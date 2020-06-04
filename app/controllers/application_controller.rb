3bb60b91cf417a963a67309a9881aa84ad752810
keisuke <keisuke6402@yahoo.co.jp>
2020-06-04 20:42:26 +0900

    user.db
    user.db作成
    まだ少しですが

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def production?
@@ -13,4 +14,11 @@ class ApplicationController < ActionController::Base
       password == Rails.application.credentials[:basic_auth][:pass]
     end
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
+
