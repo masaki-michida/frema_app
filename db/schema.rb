6e2b85193554631b9c3398712fb87bb836cf5e08
danayeon <tigerwest1009@gmail.com>
2020-06-09 00:15:30 +0900

    Generate brand-model
    brandモデルの作成
    brandテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_110724) do
+ActiveRecord::Schema.define(version: 2020_06_08_151305) do
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "sub_category_id"
+    t.index ["sub_category_id"], name: "fk_rails_22bc6bab91"
+  end
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.text "statement", null: false
+    t.bigint "category_id"
+    t.bigint "brand_id"
+    t.integer "condition", null: false
+    t.integer "delivery_fee", null: false
+    t.bigint "prefecture", null: false
+    t.integer "lag", null: false
+    t.integer "price", null: false
+    t.integer "status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "fk_rails_89fb86dc8b"
+    t.index ["user_id"], name: "fk_rails_d4b6334db2"
+  end
+
+  create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+  end
+
+  create_table "sub_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "top_category_id"
+    t.string "name"
+    t.index ["top_category_id"], name: "fk_rails_7b09ea5b1a"
+  end
+
+  create_table "top_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,4 +81,8 @@ ActiveRecord::Schema.define(version: 2020_06_04_110724) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "sub_categories"
+  add_foreign_key "items", "categories"
+  add_foreign_key "items", "users"
+  add_foreign_key "sub_categories", "top_categories"
 end
