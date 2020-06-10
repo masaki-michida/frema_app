a2988b95c889c04a3264682486a8ea3fcedc51a8
danayeon <tigerwest1009@gmail.com>
2020-06-10 09:55:01 +0900

    Update 20200608122051_create_categories.rb

--- db/migrate/20200608122051_create_categories.rb
+++ db/migrate/20200608122051_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
       t.bigint :sub_category_id
-      t.name
+      t.string :name
     end
     add_foreign_key :categories, :sub_categories
   end
