277daa6a535d5f52b29e3afd7ee3887e2515adad
danayeon <tigerwest1009@gmail.com>
2020-06-12 22:36:41 +0900

    Update items_controller.rb
    カテゴリとブランドをピックアップし、そのカテゴリ、ブランドに属する商品のみを3つずつ取得してビューに渡す処理を記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.includes(:images).where(status: 1).order(created_at: :desc).limit(6)
+    c = Item.where(status: 1).order(created_at: :desc).limit(1).category
+    b = Item.where(status: 1).order(created_at: :desc).limit(1).brand
+    @items_c = Item.includes(:images).where(status: 1).where(category: c).order(created_at: :desc).limit(3)
+    @items_b = Item.includes(:images).where(status: 1).where(brand: b).order(created_at: :desc).limit(3)
   end
 
   def show
