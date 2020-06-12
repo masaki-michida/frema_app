685d6367e95ee8ae7a389ecabb36a9a435221f00
danayeon <tigerwest1009@gmail.com>
2020-06-12 20:31:39 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.include(:images).where(status: 1).order(created_at: :desc).limit(6)
+    @items = Item.includes(:images).where(status: 1).order(created_at: :desc).limit(6)
   end
 end
