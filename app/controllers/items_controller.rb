736f91477d2e25e04162e475d74e282f5de447ac
masaki-michida <stupid763@gmail.com>
2020-06-13 23:51:31 +0900

    fix
    brand_id

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
     items_c = Item.where(status: 1).order(created_at: :desc).limit(1)
     c = items_c[0].category
     items = Item.where(status: 1).order(created_at: :desc).limit(1)
-    b = items_c[0].brand
+    b = items_c[0].brand_id
     @items_c = Item.includes(:images).where(status: 1).where(category: c).order(created_at: :desc).limit(3)
     @items_b = Item.includes(:images).where(status: 1).where(brand: b).order(created_at: :desc).limit(3)
   end
