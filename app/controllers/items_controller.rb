859e3360237212cf4781af82f71738065e35bed6
danayeon <tigerwest1009@gmail.com>
2020-06-13 22:46:06 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,9 @@
 class ItemsController < ApplicationController
   def index
-    c = Item.where(status: 1).order(created_at: :desc).limit(1).category
-    b = Item.where(status: 1).order(created_at: :desc).limit(1).brand
+    items_c = Item.where(status: 1).order(created_at: :desc).limit(1)
+    c = items_c[0].category
+    items = Item.where(status: 1).order(created_at: :desc).limit(1)
+    b = items_c[0].brand
     @items_c = Item.includes(:images).where(status: 1).where(category: c).order(created_at: :desc).limit(3)
     @items_b = Item.includes(:images).where(status: 1).where(brand: b).order(created_at: :desc).limit(3)
   end
