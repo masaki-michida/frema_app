2742a05a3a73b123f0a5e737089c51da819b11d6
masaki-michida <stupid763@gmail.com>
2020-06-14 16:32:19 +0900

    brand -> brand_id
    brandを外部キーに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,10 @@
 class ItemsController < ApplicationController
   def index
-    binding.pry
-    pick_latest_item = Item.where(status: 1).order(created_at: :desc).limit(1)
-    c = pick_latest_item[0].category
-    items = Item.where(status: 1).order(created_at: :desc).limit(1)
-    b = pick_latest_item[0].brand
+    picker = Item.where(status: 1).order(created_at: :desc).limit(1)
+    c = picker[0].category
+    b = picker[0].brand_id
     @items_c = Item.includes(:images).where(status: 1).where(category: c).order(created_at: :desc).limit(3)
-    @items_b = Item.includes(:images).where(status: 1).where(brand: b).order(created_at: :desc).limit(3)
+    @items_b = Item.includes(:images).where(status: 1).where(brand_id: b).order(created_at: :desc).limit(3)
   end
 
   def show
