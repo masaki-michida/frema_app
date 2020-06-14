8572d8a65385c723089e5d54b685d84d815bc3f5
YourName <you@example.com>
2020-06-14 09:16:30 +0900

    マイページリンク作成
    マイページリンク

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
+
   devise_for :users
   root 'items#index'
-
+  get 'users/show'
   resources :users, only: :show
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
