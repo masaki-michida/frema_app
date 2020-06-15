1c0ab4944ec32c9b8e4563ce5ca57e1d4cb3f303
masaki-michida <stupid763@gmail.com>
2020-06-15 13:02:37 +0900

    change con route
    root_pathに変更
    ルーティング変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    user_path(resource.id)
+    root_path
   end
 
   def basic_auth
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  get 'users/show'
+  resources :users, only: :show
   resources :items
 end
