62baf85d24b52063e587a05936b8cee0d9fd8be8
masaki-michida <stupid763@gmail.com>
2020-06-15 12:08:14 +0900

    routeing
    ルーティング変更
    コンフリクト解消

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   get 'users/show'
-  resources :users, only: :show
-  resources :items, except: :show
+  resources :items
 end
