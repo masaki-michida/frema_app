b38dec270f6a941cd455a64d8e783645dc35d459
danayeon <tigerwest1009@gmail.com>
2020-06-15 17:10:32 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,11 +40,10 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to root_path
+      redirect_to item_path(params[:id])
     else
       render :new
     end
-    redirect_to item_path(params[:id])
   end
 
   def destroy
