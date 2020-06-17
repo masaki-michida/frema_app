00cdee184bbcdd4931adb5cb475876a39a65fdac
danayeon <tigerwest1009@gmail.com>
2020-06-18 06:27:06 +0900

    Update routes.rb
    cardのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :users, only: :show
   resources :items
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 end
