521289489f97a09b225f9a5e93787726749940df
YourName <you@example.com>
2020-06-18 12:22:10 +0900

    mypage編集
     - レイアウト編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    @items = Item.where(user_id: current_user.id)
     # @user = User.find(params[:id])
   end
 end
