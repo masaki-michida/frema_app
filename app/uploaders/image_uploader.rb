4de49f1867b0d54ec5f61f9d8a3bb1ebfeae93d1
masaki-michida <stupid763@gmail.com>
2020-06-19 01:51:45 +0900

    change storage
    ローカル環境の場合は
    /public/uploads/image/content
    に画像を保存使用
    本番環境ではS3を使用

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,8 +6,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [100, 100]
 
   # Choose what kind of storage to use for this uploader:
-  # if Rails.env.production? || Rails.env.staging?
+  if Rails.env.production? || Rails.env.staging?
     storage :fog
+  elsif Rails.env.development?
+    storage :file
+  end
     
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
