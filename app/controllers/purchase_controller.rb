f97e57a6c9c27bc19a7b33027d2da10c9a39b424
masaki-michida <stupid763@gmail.com>
2020-06-19 18:54:31 +0900

    controller
    APIキーをcredentialsからに変更

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -2,11 +2,12 @@ class PurchaseController < ApplicationController
   require 'payjp'
 
   def index
+    # binding.pry
     card = Card.where(user_id: current_user.id).first
     if card.blank?
       redirect_to controller: "card", action: "new"
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = Rails.application.credentials.payjp[:payjp_private_key]
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
@@ -14,7 +15,7 @@ class PurchaseController < ApplicationController
 
   def pay
     card = Card.where(user_id: current_user.id).first
-    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    Payjp.api_key = Rails.application.credentials.payjp[:payjp_private_key]
     Payjp::Charge.create(
     :amount => 13500,
     :customer => card.customer_id,
