e9b9c083b3c4d59af67fb81e4a4ccba646903d3e
danayeon <tigerwest1009@gmail.com>
2020-06-19 19:00:09 +0900

    Update

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -10,14 +10,16 @@ class PurchaseController < ApplicationController
       Payjp.api_key = Rails.application.credentials.payjp[:payjp_private_key]
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
+      @item = Item.find(params[:item][item_id])
     end
   end
 
   def pay
+    @item = Item.find(prams[:item][item_id])
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = Rails.application.credentials.payjp[:payjp_private_key]
     Payjp::Charge.create(
-    :amount => 13500,
+    :amount => @item.pricce,
     :customer => card.customer_id,
     :currency => 'jpy',
   )
