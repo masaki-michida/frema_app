1220d1d1135dc5eb943346699aeee12cbeb2e914
danayeon <tigerwest1009@gmail.com>
2020-06-21 16:50:26 +0900

    try2

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,18 +1,8 @@
-$('#image-box').on('change', '.js-file', function(e) {
-  const targetIndex = $(this).parent().data('index');
-  // ファイルのブラウザ上でのURLを取得する
-  const file = e.target.files[0];
-  const blobUrl = window.URL.createObjectURL(file);
-
-  // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
-  if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
-    img.setAttribute('src', blobUrl);
-  } else {  // 新規画像追加の処理
-    $('#previews').append(buildImg(targetIndex, blobUrl));
-    // fileIndexの先頭の数字を使ってinputを作る
-    $('#image-box').append(buildFileField(fileIndex[0]));
-    fileIndex.shift();
-    // 末尾の数に1足した数を追加する
-    fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
-  }
+$(document).on('turbolinks:load', ()=> {
+  $('#image-box').on('change', function (e) {
+    var file = e.target.files[0];
+    var blobUrl = window.URL.createObjectURL(file);
+    var img = document.getElementById('0');
+    img.src = blobUrl;
 });
+}
