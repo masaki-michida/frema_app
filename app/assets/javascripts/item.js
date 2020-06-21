c43a68154ef365de1e22021698efd20409015763
danayeon <tigerwest1009@gmail.com>
2020-06-21 16:53:21 +0900

    try3

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load', ()=> {
   $('#image-box').on('change', function (e) {
     var file = e.target.files[0];
     var blobUrl = window.URL.createObjectURL(file);
-    var img = document.getElementById('0');
+    var img = document.getElementById(0);
     img.src = blobUrl;
 });
 }
