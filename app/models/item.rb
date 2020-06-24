9956759a901a298738cf14acbd14cdb95caafe71
danayeon <tigerwest1009@gmail.com>
2020-06-24 12:57:50 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ApplicationRecord
   validates :name, :status, :price, :statement, :category_id, :prefecture_id, :user_id ,:condition, :delivery_fee , :lag, presence: true
 
   validates_associated :images
+  validates :images, presence: true
 
   accepts_nested_attributes_for :images, allow_destroy: true
 end
